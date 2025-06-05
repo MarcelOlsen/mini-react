d1ca9dcf4c0b43b0101b29a13dc50afce682bff0
MarcelOlsen <marcelolsen26@gmail.com>
2025-06-06 01:05:34 +0200

    :coffin:  chore: removed forgotten starter code

--- index.ts
+++ index.ts
@@ -1,12 +1,2 @@
 export { createElement, render } from "./src/MiniReact";
-export type {
-	FunctionalComponent,
-	MiniReactElement,
-	AnyMiniReactElement,
-} from "./src/types";
-
-const hello = () => {
-	console.log("Hello via Bun!");
-};
-
-export default hello;
+export * from "./src/types";
