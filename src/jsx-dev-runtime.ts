a2c4f0095370a3793b9e1fe5303e0b337ec0f50e
MarcelOlsen <marcelolsen26@gmail.com>
2025-06-22 21:57:41 +0200

    📦 feat: add JSX runtime and dev runtime exports

---
+++ src/jsx-dev-runtime.ts
@@ -0,0 +1,6 @@
+/* ***************** */
+/* JSX Dev Runtime   */
+/* ***************** */
+
+// Export development JSX runtime functions
+export { jsxDEV as jsx, jsxDEV as jsxs, Fragment } from "./MiniReact"; 
---
+++ src/jsx-runtime.ts
@@ -0,0 +1,9 @@
+/* ***************** */
+/* JSX Runtime       */
+/* ***************** */
+
+// Re-export JSX runtime functions for build tool integration
+export { jsx, jsxs, Fragment } from "./MiniReact";
+
+// Export development version
+export { jsxDEV } from "./MiniReact"; 
